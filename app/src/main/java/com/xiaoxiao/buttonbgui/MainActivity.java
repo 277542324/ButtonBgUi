a247e77568b6592aec5a13aba125f802201f9ae4
xiaoxiao <277542324@qq.com>
2018-01-05 09:46:31 +0800

    第一次提交按钮圆角

---
+++ app/src/main/java/com/xiaoxiao/buttonbgui/MainActivity.java
@@ -0,0 +1,19 @@
+package com.xiaoxiao.buttonbgui;
+
+import android.support.v4.content.ContextCompat;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.came.viewbguilib.ButtonBgUi;
+
+/**
+ * @author Administrator
+ */
+public class MainActivity extends AppCompatActivity {
+    private ButtonBgUi bct;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
